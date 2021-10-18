e123e5e558f7ce9e974ac90a821a65115c4128f2
kainy01 <kainykawai@gmail.com>
2021-10-18 15:41:38 +0300

    first commit

---
+++ myscripts.js
@@ -0,0 +1,8 @@
+
+function func() {
+  document.getElementById("true").style.color = "#ffffff";
+  document.getElementById("true").style.backgroundColor = "#70c04d";
+  document.getElementById("icon").className = "fas fa-check-circle";
+  var el = document.getElementById("b");
+  el.remove();
+}
